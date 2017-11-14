8e8ec9b7362911b6728dda31db2bcfb961c5f89a
David <david.dexter.charles@gmail.com>
2017-11-14 13:50:57 +0000

    updates

---
+++ client.py
@@ -0,0 +1,18 @@
+from azure.servicebus import ServiceBusService, Message, Queue
+bus_service = ServiceBusService(
+    service_namespace='cloudassignment34ed0', 
+    shared_access_key_name='RootManageSharedAccessKey',
+    shared_access_key_value='P6TUMCQVFg8ZIG8Z5KiPAIFaAHzvTcX9g7n8fNYAbZ0=')
+
+bus_service.create_queue('taskqueue')
+msg = Message('Test Message')
+bus_service.send_queue_message('taskqueue', msg)
+
+# bus_service_r = ServiceBusService(
+#     service_namespace='cloudassignment34ed0', 
+#     shared_access_key_name='RootManageSharedAccessKey',
+#     shared_access_key_value='P6TUMCQVFg8ZIG8Z5KiPAIFaAHzvTcX9g7n8fNYAbZ0=')
+
+# msg = bus_service.receive_queue_message('taskqueue', peek_lock=False)
+# print(msg.body)
+ 
---
+++ workserver.py
@@ -0,0 +1,62 @@
+import math
+import socket
+import threading
+import time
+
+from bottle import route, run
+
+hostname = socket.gethostname()
+hostport = 9000
+keepworking = False  
+
+
+
+def workerthread():
+    while (True):
+        while (keepworking == True):
+            for x in range(1, 69):
+                y = math.factorial(x)
+        time.sleep(3)
+
+
+# start the worker thread
+worker_thread = threading.Thread(target=workerthread, args=())
+worker_thread.start()
+
+
+def writebody():
+    body = '<html><head><title>Work interface - build</title></head>'
+    body += '<body><h2>Worker interface on ' + hostname + '</h2><ul><h3>'
+
+    if keepworking == False:
+        body += '<br/>Worker thread is not running. <a href="./do_work">Start work</a><br/>'
+    else:
+        body += '<br/>Worker thread is running. <a href="./stop_work">Stop work</a><br/>'
+
+    body += '<br/>Usage:<br/><br/>/do_work = start worker thread<br/>/stop_work = stop worker thread<br/>'
+    body += '</h3></ul></body></html>'
+    return body
+
+
+@route('/')
+def root():
+    return writebody()
+
+
+@route('/do_work')
+def do_work():
+    global keepworking
+    # start worker thread
+    keepworking = True
+    return writebody()
+
+
+@route('/stop_work')
+def stop_work():
+    global keepworking
+    # stop worker thread
+    keepworking = False
+    return writebody()
+
+
+run(host=hostname, port=hostport)

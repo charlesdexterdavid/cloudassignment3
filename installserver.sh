9418c8dc87c2631a2883f5a3ab4dc10df9695f4b
unknown <david.dexter.charles@gmail.com>
2017-11-19 14:11:47 -0400

    updates to message count for receiving

--- installserver.sh
+++ installserver.sh
@@ -7,8 +7,6 @@ cp MsgGenRecv.jar $workserver_path
 
 # install python3-bottle 
 apt -y update
-pip3 install azure-storage
-pip3 install azure-servicebus
 apt -y install default-jre
 apt -y install openjdk-8-jdk
 
@@ -17,9 +15,8 @@ touch /etc/systemd/system/workserver.service
 printf '[Unit]\nDescription=workServer Service\nAfter=rc-local.service\n' >> /etc/systemd/system/workserver.service
 printf '[Service]\nWorkingDirectory=%s\n' $workserver_path >> /etc/systemd/system/workserver.service
 
-#printf 'ExecStart=/usr/bin/python3 %s/workserver.py\n' $workserver_path >> /etc/systemd/system/workserver.service
 
-printf 'ExecStart=/usr/bin/java -jar %s/MsgGenRecv.jar receive_messages taskqueue 700\n' $workserver_path >> /etc/systemd/system/workserver.service
+printf 'ExecStart=/usr/bin/java -jar %s/MsgGenRecv.jar receive_messages taskqueue 250\n' $workserver_path >> /etc/systemd/system/workserver.service
 
 printf 'ExecReload=/bin/kill -HUP $MAINPID\nKillMode=process\nRestart=on-failure\n' >> /etc/systemd/system/workserver.service
 printf '[Install]\nWantedBy=multi-user.target\nAlias=workserver.service' >> /etc/systemd/system/workserver.service

78fac289c22ac1beebf072d79a9f6cc8cb5896f6
David Charles <david.charles@my.uwi.edu>
2017-11-19 21:22:46 -0400

    Update installserver.sh

--- installserver.sh
+++ installserver.sh
@@ -16,7 +16,7 @@ printf '[Unit]\nDescription=workServer Service\nAfter=rc-local.service\n' >> /et
 printf '[Service]\nWorkingDirectory=%s\n' $workserver_path >> /etc/systemd/system/workserver.service
 
 
-printf 'ExecStart=/usr/bin/java -jar %s/MsgGenRecv.jar receive_messages taskqueue 400\n' $workserver_path >> /etc/systemd/system/workserver.service
+printf 'ExecStart=/usr/bin/java -jar %s/MsgGenRecv.jar receive_messages taskqueue 600\n' $workserver_path >> /etc/systemd/system/workserver.service
 
 printf 'ExecReload=/bin/kill -HUP $MAINPID\nKillMode=process\nRestart=on-failure\n' >> /etc/systemd/system/workserver.service
 printf '[Install]\nWantedBy=multi-user.target\nAlias=workserver.service' >> /etc/systemd/system/workserver.service
